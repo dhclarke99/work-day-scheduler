ed1d47d0efeccd2b528812d815263a67cd50b58f
dhclarke99 <dhclarke99@gmail.com>
2023-03-30 22:57:09 -0400

    added funtion to save text inputs to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,22 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 var submitButton = $('.btn')
+var eventText = $('textarea')
+console.log(eventText)
+
+function readEventsFromStorage() {
+  var event = localStorage.getItem('event');
+  if (event) {
+    event = JSON.parse(event);
+  } else {
+    event = [];
+  }
+  return event;
+}
+
+function saveEventsToStorage(event) {
+  localStorage.setItem('event', JSON.stringify(event));
+}
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -12,6 +28,21 @@ $(function () {
 
     submitButton.on('click', function(){
       console.log("button test")
+      
+
+      function handleSubmit() {
+        
+      
+        // read user input from the form
+        var eventName = eventText.val().trim();
+       console.log(eventName);
+        var newEvent = eventName;
+      
+        // add project to local storage
+        var event = readEventsFromStorage();
+        event.push(newEvent);
+        saveEventsToStorage(event);
+      } handleSubmit()
     })
     //
     // TODO: Add code to apply the past, present, or future class to each time
@@ -25,5 +56,9 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+    setInterval(function() {
+    var time = dayjs().format('hh:mm:ss a');
+    $('#currentDay').text(time);
+    },1000)
   });
   
